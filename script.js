74cc9d801ae87d0297d8db9344263cc41383a461
AlbertoSilvEn <aalbertosilvae@gmail.com>
2023-09-05 23:05:41 -0600

    Se afrefan cambios al formulario y se agrega hoja de noticias dentales con su hoja de estilos

---
+++ script.js
@@ -0,0 +1,26 @@
+var nombreMedico = "";
+var nombrePaciente = "";
+
+var edad = ;
+var cantidadProducto = ;
+
+var cuponDescuento = false;
+var pacienteNuevo = true;
+
+var fechaCita;
+var horaCita;
+
+var edad = null;
+var nombreMedico = null;
+
+
+var cantidadProdcutocadena = peso.toString();
+console.log (cantidadProdcutocadena);
+console.log(typeof(cantidadProdcutocadena); 
+
+
+var edadNumero = parseInt(edad)
+console.log (edadNumero);
+console.log (typeof(edadNumero));
+
+
